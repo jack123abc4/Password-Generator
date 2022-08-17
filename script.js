5b82e9749162168031c2a682583f63cd122bdb49
jack123abc4 <jack.r.motherway@gmail.com>
2022-08-17 15:37:47 -0400

    added arrays

--- script.js
+++ script.js
@@ -20,10 +20,33 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function intToChar(int) {
-  const code = 'a'.charCodeAt(0);
+  const code = 'A'.charCodeAt(0);
   return String.fromCharCode(code + int);
 }
 
-console.log(intToChar(0));
-console.log(intToChar(15));
-console.log(intToChar(30));
+
+// generate alphabetic chars
+var lowerChars = [];
+var upperChars = [];
+for (var i = 'a'.charCodeAt(0); i < 'a'.charCodeAt(0) + 26; i++) {
+  lowerChars.push(String.fromCharCode(i)); // converts unicode to lowercase alpha char
+  upperChars.push(String.fromCharCode(i).toUpperCase()); // converts unicode to lowercase alpha char, makes it uppercase
+}
+
+// generate numeric chars
+var numChars = [];
+for (var i = 0; i < 10; i++) {
+  numChars.push(i); // adds chars 0-9
+}
+
+// generate special chars
+var specialString = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' // string containing all special chars, using \ to escape single quote and backslash
+var specialChars = [];
+for (var i = 0; i < specialString.length; i++) {
+  specialChars.push(specialString.charAt(i)); // adds each char of special string to array
+}
+
+console.log(lowerChars);
+console.log(upperChars);
+console.log(numChars);
+console.log(specialChars);
